8d56e6f55fa495e054e3697738a070e218456217
Paul Kenedy <pkenedy48@gmail.com>
2024-10-30 07:51:40 +0300

    pages updates

---
+++ build/CMakeFiles/LogFileProcessor.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LogFileProcessor.dir/LogProcessor.cpp.o"
+  "CMakeFiles/LogFileProcessor.dir/LogProcessor.cpp.o.d"
+  "CMakeFiles/LogFileProcessor.dir/main.cpp.o"
+  "CMakeFiles/LogFileProcessor.dir/main.cpp.o.d"
+  "LogFileProcessor.exe"
+  "LogFileProcessor.pdb"
+  "libLogFileProcessor.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LogFileProcessor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
